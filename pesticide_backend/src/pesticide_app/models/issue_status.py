80512e891db487297c15566e3ed93172dc8e15a8
MihirSachdeva <mihirsachdeva.ms@gmail.com>
2020-09-29 20:23:33 +0530

    Add a default issue status

--- pesticide_backend/src/pesticide_app/models/issue_status.py
+++ pesticide_backend/src/pesticide_app/models/issue_status.py
@@ -12,5 +12,13 @@ class IssueStatus(models.Model):
     type = models.CharField(max_length=30, choices=IssueStatusType.choices,
                             default=IssueStatusType.PENDING)
 
+    @classmethod
+    def get_default_issue_status(cls):
+        issue_status, created = cls.objects.get_or_create(
+            status_text='New',
+            defaults=dict(color='#217bf3', type=cls.IssueStatusType.PENDING)
+        )
+        return issue_status.pk
+
     def __str__(self):
         return self.status_text
