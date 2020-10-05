8720004c0728710c00fb70720b211b247dd1dcb1
MihirSachdeva <mihirsachdeva.ms@gmail.com>
2020-10-05 22:09:04 +0530

    Fix backend static files and media files display issue

--- pesticide_backend/src/pesticide/settings.py
+++ pesticide_backend/src/pesticide/settings.py
@@ -109,7 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend-static/')
 
 ASGI_APPLICATION = 'pesticide.routing.application'
 
